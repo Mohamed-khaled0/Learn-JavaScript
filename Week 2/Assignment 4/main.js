3d953d30e0300e43aacf55dcf1694507049f15a7
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-09-02 14:26:51 +0300

    week 2

--- Week 2/Assignment 4/main.js
+++ Week 2/Assignment 4/main.js
@@ -1,7 +1,7 @@
 let a = 21;
 let b = 20;
 let cc = (d = a);
-let aa = (e = b);
-let c = d + "" + e;
+let aa = (e = a);
+let c = e + "" +d;
 
-console.log(`_${d}_${(c + "_").repeat(++cc % --aa)} ${++e}_`);
+console.log(`_${d}_${(c + "_").repeat(++cc % --aa)} ${--e}_`);
