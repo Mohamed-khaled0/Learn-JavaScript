9748a00276678fe3b3343e2539a3e8594f441a83
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-09-18 11:59:05 +0300

    Week 19 (111 - 114 )

--- Week19/Assignment 2/main.js
+++ Week19/Assignment 2/main.js
@@ -1,9 +1,22 @@
-let num1 = 10;
-let num2 = 20;
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let select = document.querySelector("#select");
 
-console.log(num1 < num2); // true
-console.log(num1 !== num2); // true
-console.log(num1 < num2 && num1 >1); // true
-console.log(typeof num1 === typeof num2); // true
-console.log(num1 <= num2); // true
-console.log(num1 != num2); // true
+one.oninput = function(){
+    window.sessionStorage.setItem("text-1", one.value);
+}
+two.oninput = function(){
+    window.sessionStorage.setItem("text-2", two.value);
+}
+three.oninput = function(){
+    window.sessionStorage.setItem("text-3", three.value);
+}
+select.onchange = function(){
+    window.sessionStorage.setItem("select", select.value);
+}
+
+one.value = window.sessionStorage.getItem("text-1");
+two.value = window.sessionStorage.getItem("text-2");
+three.value = window.sessionStorage.getItem("text-3");
+select.value = window.sessionStorage.getItem("select");
